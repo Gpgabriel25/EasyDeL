1ccf7cfef4a2f18af504bb51eb860dc055c3fa26
Gabriel Garcia <66880445+Gpgabriel25@users.noreply.github.com>
2026-06-06 21:13:47 -0400

    fix: allow ejkernel 0.0.80.3 in version assert

--- easydel/__init__.py
+++ easydel/__init__.py
@@ -1425,7 +1425,7 @@ else:
     )
 
     _targeted_eformer_versions = ["0.0.99.12", "0.0.101"]
-    _targeted_ejkernel_versions = ["0.0.78", "0.0.79"]
+    _targeted_ejkernel_versions = ["0.0.78", "0.0.79", "0.0.80.3"]
 
     from eformer import __version__ as _eform_version
     from ejkernel import __version__ as _ejker_version
